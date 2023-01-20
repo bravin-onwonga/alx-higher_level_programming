0a76b810ac35abb07ae2a94dc46a34f9c0c41527
bravin-onwonga <onbravin@gmail.com>
2023-01-20 18:30:08 +0300

    Pair of numbers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for n1 in range(9):
+    for n2 in range(10):
+        if n1 == 8 and n2 == 9:
+            print("{}{}".format(n1, n2))
+        elif n1 != n2:
+            print("{}{}".format(n1, n2), end=", ")
