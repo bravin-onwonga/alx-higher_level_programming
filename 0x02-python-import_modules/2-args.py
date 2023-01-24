c1299349524250048dcdb2a3377cc68b7a622525
bravin-onwonga <onbravin@gmail.com>
2023-01-24 19:03:36 +0300

    Make a script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         print("{} arguments:".format(len))
 
     for i in range(1, len + 1):
-        print("{} {}:".format(i, argv[i])
+        print("{}: {}".format(i, argv[i]))
