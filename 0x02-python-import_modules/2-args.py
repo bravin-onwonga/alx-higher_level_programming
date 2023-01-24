7c82fec2daa786da081d841bbcd7a0d095bcd20e
bravin-onwonga <onbravin@gmail.com>
2023-01-24 18:51:34 +0300

    Make a script dynamic!

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv:
+        len = len(argv) - 1
+
+        if len == 0:
+            print("{} arguments.".format(len))
+        elif len == 1:
+            print("{} argument:".format(len))
+        else:
+            print("{} arguments:".format(len))
+
+        for i in range(1, len + 1):
+            print("{} {}:".format(i, argv[i - 1])
