7020ac1597e06204ca19022b2b90ad0a9a086581
bravin-onwonga <onbravin@gmail.com>
2023-01-26 18:01:28 +0300

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,18 +8,17 @@ if __name__ == "__main__":
     if len < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    n1 = int(agrv[0])
-    n2 = int(argv[2])
-    op = argv[1]
-    elif op != "+" or op != "-" or op != "*" or op != "/":
+    n1 = int(agrv[1])
+    n2 = int(argv[3])
+    op = argv[2]
+    if op != "+" or op != "-" or op != "*" or op != "/":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+    if op == "+":
+        print("{} {} {} = {}".format(n1, op, n2, add(n1, n2)))
+    elif op == "-":
+        print("{} {} {} = {}".format(n1, op, n2, sub(n1, n2)))
+    elif op == "*":
+        print("{} {} {} = {}".format(n1, op, n2, mul(n1, n2)))
     else:
-        if op == "+":
-            print("{} {} {} = {}".format(n1, op, n2, add(n1, n2)))
-        elif op == "-":
-            print("{} {} {} = {}".format(n1, op, n2, sub(n1, n2)))
-        elif op == "*":
-            print("{} {} {} = {}".format(n1, op, n2, mul(n1, n2)))
-        else:
-            print("{} {} {} = {}".format(n1, op, n2, div(n1, n2)))
+        print("{} {} {} = {}".format(n1, op, n2, div(n1, n2)))
