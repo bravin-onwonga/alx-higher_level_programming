d7202b18e67d7c65b77136580a42618ff5b9d193
bravin-onwonga <onbravin@gmail.com>
2023-02-01 17:11:55 +0300

    Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_str = ""
+    len_str = len(my_string)
+
+    for i in range(len_str):
+        if my_string[i] == "c" or my_string[i] == "C":
+            my_string[i] == ""
+        new_str = new_str.join(my_string[i])
+    return new_str
+
+
+if __name__ == "__main__":
+    no_c()
