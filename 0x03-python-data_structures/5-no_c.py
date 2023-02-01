c404db04bb943cc64041e16acbd33f48212556eb
bravin-onwonga <onbravin@gmail.com>
2023-02-01 17:13:17 +0300

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -8,7 +8,7 @@ def no_c(my_string):
         if my_string[i] == "c" or my_string[i] == "C":
             my_string[i] == ""
         new_str = new_str.join(my_string[i])
-    return new_str
+        return new_str
 
 
 if __name__ == "__main__":
