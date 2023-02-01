13087c5db6bb4a3e0bd92025a7ab14c57883a61e
bravin-onwonga <onbravin@gmail.com>
2023-02-01 16:50:55 +0300

    Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def _cpy_list(my_list):
+    len_list = len(my_list)
+    list_copy = []
+
+    for i in range(len_list):
+        list_copy.append(my_list[i])
+
+    return list_copy
+
+
+def new_in_list(my_list, idx, element):
+    len_my_list = len(my_list) - 1
+
+    if idx > len_my_list:
+        _cpy_list(my_list)
+    if idx < 0:
+        _cpy_list(my_list)
+    else:
+        my_list[idx] = element
+        _cpy_lsit(my_list)
+
+
+if __name__ == "__main__":
+    new_in_list()
