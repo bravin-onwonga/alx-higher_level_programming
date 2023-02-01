d20738e7aada703c08306023394ec3e3d59bac20
bravin-onwonga <onbravin@gmail.com>
2023-02-01 23:47:58 +0300

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+
+    len_lst = len(my_list)
+
+    for i in range(len_lst):
+        if my_list[i] > my_list[i + 1]:
+            tmp = my_list[i + 1]
+            my_list[i + 1] = my_list[i]
+            my_list[i] = tmp
+    return my_list[i]
