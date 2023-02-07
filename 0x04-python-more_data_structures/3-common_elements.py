022f5a29d925c0221783200fc4d3fa7150e4b480
bravin-onwonga <onbravin@gmail.com>
2023-02-07 18:24:35 +0300

    Present in both

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    common = list(map(lambda x, y: x == y, set_1, set_2))
+    lst = []
 
-    return common
+    for x in set_1:
+        for y in set_2:
+            if x == y:
+                lst.append(x)
+
+    return lst
