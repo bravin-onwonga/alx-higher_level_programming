2434a531e63a9b46d6b5fb3c1d12ccc0618e2d83
bravin-onwonga <onbravin@gmail.com>
2023-02-07 20:50:43 +0300

    Make executable

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,5 +4,5 @@ def print_sorted_dictionary(a_dictionary):
     sorted_dict = sorted(a_dictionary)
     
     for i in sorted_dict:
-        print(i + " : " ,a_dictionary[i])
+        print("{}: {}".format(i ,a_dictionary[i]))
     
