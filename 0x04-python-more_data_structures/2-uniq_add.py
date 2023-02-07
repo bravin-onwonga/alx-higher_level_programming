f7b7294b476c373115cd3284db19a4c5c196ec8c
bravin-onwonga <onbravin@gmail.com>
2023-02-07 17:49:33 +0300

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    unique_list = {my_list}
     sum = 0
+    unique_list = {}
+    
+    for i in my_list:
+        unique_list.append(i)
 
     for i in unique_list:
         sum = sum + i
