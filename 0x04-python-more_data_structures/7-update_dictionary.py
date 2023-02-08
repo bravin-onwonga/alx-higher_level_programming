5cb623301e44ee2245517367130281cd691f7a30
bravin-onwonga <onbravin@gmail.com>
2023-02-08 16:04:29 +0300

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    new_dict = {a_dictionary}
+    new_dict = {}
+
+    for i in a_dictionary:
+        new_dict[i] = a_dictionary[i]
+
     new_dict[key] = value
     print(new_dict)
