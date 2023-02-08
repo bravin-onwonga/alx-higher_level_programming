e885eb248636162d8500777ea27339bb67a33159
bravin-onwonga <onbravin@gmail.com>
2023-02-08 20:40:45 +0300

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,9 @@ def roman_to_int(roman_string):
     roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50,
                   "C": 100, "D": 500, "M": 1000}
 
+    if roman_string is None:
+        return None
+
     len_roman = len(roman_string)
     result = 0
     i = 0
