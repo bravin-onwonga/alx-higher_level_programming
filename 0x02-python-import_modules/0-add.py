6c5237198b5895f6b8dc49857ea717772250bb0b
bravin-onwonga <onbravin@gmail.com>
2024-01-04 20:34:58 +0300

    Executing the add module as a script

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    add(a, b)
 
-res = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, res))
+    res = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, res))
