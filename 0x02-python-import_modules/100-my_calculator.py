6483b2a6bc1b19d88b4d50b9a8f269f4141a34c0
bravin-onwonga <onbravin@gmail.com>
2024-01-05 01:18:39 +0300

    Importing sys after variable __name__

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-import sys
 
-arg_list = sys.argv
-arg_len = len(arg_list)
+if __name__ == "__main__":
+    import sys
+    arg_list = sys.argv
+    arg_len = len(arg_list)
 
-if (arg_len != 4):
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    if (arg_len != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(arg_list[1])
+    b = int(arg_list[3])
+    op = arg_list[2]
 
-a = int(arg_list[1])
-b = int(arg_list[3])
-op = arg_list[2]
-
-if (op == "+"):
-    if __name__ == "__main__":
+    if (op == "+"):
         res = add(a, b)
 
-        print("{:d} + {:d} = {:d}".format(a, b, res))
+        print("{:d} {} {:d} = {:d}".format(a, op, b, res))
 
-elif (op == "-"):
-    if __name__ == "__main__":
+    elif (op == "-"):
         res = sub(a, b)
 
-        print("{:d} - {:d} = {:d}".format(a, b, res))
+        print("{:d} {} {:d} = {:d}".format(a, op, b, res))
 
-elif (op == "*"):
-    if __name__ == "__main__":
+    elif (op == "*"):
         res = mul(a, b)
 
-        print("{:d} * {:d} = {:d}".format(a, b, res))
+        print("{:d} {} {:d} = {:d}".format(a, op, b, res))
 
-elif (op == "-"):
-    if __name__ == "__main__":
+    elif (op == "-"):
         res = div(a, b)
 
-        print("{:d} / {:d} = {:d}".format(a, b, res))
+        print("{:d} {} {:d} = {:d}".format(a, op, b, res))
 
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
