0718d22a3272c4e85951ef08e0fc2b3a10d84aaa
bravin-onwonga <onbravin@gmail.com>
2024-01-04 22:06:40 +0300

    Making sure it is not executed when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 import sys
 
-arg_list = sys.argv
-arg_len = len(arg_list)
-if arg_len == 1:
-	print("0 arguments.")
+if __name__ == "__main__":
+	arg_list = sys.argv
+	arg_len = len(arg_list)
+	if arg_len == 1:
+		print("0 arguments.")
 
-elif arg_len == 2:
-	print("1 argument:")
-	print("1: {}".format(arg_list[1]))
+	elif arg_len == 2:
+		print("1 argument:")
+		print("1: {}".format(arg_list[1]))
 
-else:
-	print("{} arguments:".format(arg_len - 1))
-	i = 1
-	while (i < arg_len):
-		print("{}: {}".format(i, arg_list[i]))
-		i  += 1
+	else:
+		print("{} arguments:".format(arg_len - 1))
+		i = 1
+		while (i < arg_len):
+			print("{}: {}".format(i, arg_list[i]))
+			i  += 1
