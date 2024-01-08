09934c81d15332f6a64035562d2a93ccf18c8807
bravin-onwonga <onbravin@gmail.com>
2024-01-08 22:48:10 +0300

    Solving for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    last_index = len(my_list) - 1
-
-    if (last_index < 0):
+    if (my_list == []):
         print()
+
+    last_index = len(my_list) - 1
 
     while (last_index >= 0):
         print("{:d}".format(my_list[last_index]))
