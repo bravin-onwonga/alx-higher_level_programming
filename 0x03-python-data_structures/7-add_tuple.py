fbcd1cc1848864ed6c836f27c8df68fdfb5ddb66
bravin-onwonga <onbravin@gmail.com>
2024-01-08 21:45:15 +0300

    Long route

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,19 +3,27 @@ def add_tuple(tuple_a=(), tuple_b=()):
     len_tuple_a = len(tuple_a)
     len_tuple_b = len(tuple_b)
 
+    a1 = 0
+    a2 = 0
+    b1 = 0
+    b2 = 0
+
+    if len_tuple_a > 1:
+        a1 = tuple_a[0]
+        a2 = tuple_a[1]
+
+    if len_tuple_b > 1:
+        b1 = tuple_b[0]
+        b2 = tuple_b[1]
+
     if len_tuple_a == 1:
-        tuple_a = (tuple_a[0], 0)
+        a1 = tuple_a[0]
+        a2 = 0
 
-    elif len_tuple_b == 1:
-        tuple_b = (tuple_a[1], 0)
+    if len_tuple_b == 1:
+        b1 = tuple_b[0]
+        b2 = 0
 
-    elif len_tuple_a == 0:
-        tuple_a = (0, 0)
+    new_tuple = (a1 + b1, a2 + b2)
 
-    elif len_tuple_b == 0:
-        tuple_b = (0, 0)
-
-    a = tuple_a[0] + tuple_b[0]
-    b = tuple_a[1] + tuple_b[1]
-
-    return a, b
+    return new_tuple
