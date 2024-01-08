f90d097589a1ba5fbafe74b28b15d35d3c3bffe1
bravin-onwonga <onbravin@gmail.com>
2024-01-08 23:01:04 +0300

    Solving failing test cases

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    last_index = len(my_list) - 1
+    if my_list is not None:
+        last_idx = len(my_list) - 1
 
-    while (last_index >= 0):
-        print("{:d}".format(my_list[last_index]))
-        last_index -= 1
+        while (last_idx >= 0):
+            print("{:d}".format(my_list[last_idx]))
+            last_idx -= 1
