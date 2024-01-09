77da58f741922aaa075e12411b089c2466d59df1
bravin-onwonga <onbravin@gmail.com>
2024-01-09 23:11:06 +0300

    Check if Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,10 +1,17 @@
 #include "lists.h"
 
+/**
+ * is_palindrome - checks if a singly-list is a palindrome
+ *
+ * @head: head node
+ * Return: 1 if palindrome; otherwise 0
+ */
+
 int is_palindrome(listint_t **head)
 {
 	listint_t *curr, *temp;
-	int len_list = 0, i = 0, k = 0;
-	char *arr;
+	int len_list = 0, i = 0, k = 0, mid_point;
+	int *arr;
 
 	if (*head == NULL)
 		return (1);
@@ -16,7 +23,10 @@ int is_palindrome(listint_t **head)
 		curr = curr->next;
 	}
 
-	arr = malloc(sizeof(char *) * len_list);
+	arr = malloc(sizeof(int) * (len_list - 1));
+	if (arr == NULL)
+		exit(EXIT_FAILURE);
+
 	temp = *head;
 	while (i <= len_list && temp != NULL)
 	{
@@ -25,15 +35,18 @@ int is_palindrome(listint_t **head)
 		i++;
 	}
 	i--;
+	mid_point = i / 2;
 
-	while (k < i / 2)
+	while (k <= mid_point)
 	{
 		if (arr[k] != arr[i])
 		{
+			free(arr);
 			return (0);
 		}
 		k++;
 		i--;
 	}
+	free(arr);
 	return (1);
 }
---
+++ 0x03-python-data_structures/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <listobject.h>
+#include <object.h>
+
+void print_python_list_info(PyObject *p);
+
+#endif /* MAIN_H */
