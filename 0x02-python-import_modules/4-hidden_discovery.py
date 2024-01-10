c0243b037e6270d73de572373706ae461135dd55
bravin-onwonga <onbravin@gmail.com>
2024-01-11 00:27:39 +0300

    First try three

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden
     defined_modules = [name for name in dir(hidden)]
     for s in defined_modules:
-        if name[0] is not '_' and name[1] is not '_':
+        if name[0] != '_' and name[1] != '_':
             print(name)
