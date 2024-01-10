ca370929133b79fa073b286ee6a3c46b19e104cc
bravin-onwonga <onbravin@gmail.com>
2024-01-10 15:19:13 +0300

    Operations on a python dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    new_list = sorted(a_dictionary)
+    if a_dictionary:
+        new_list = sorted(a_dictionary)
 
-    for s in new_list:
-        print("{} : {}".format(s, a_dictionary[s]))
+        for s in new_list:
+            print("{} : {}".format(s, a_dictionary[s]))
