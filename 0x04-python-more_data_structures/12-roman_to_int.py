fd38fef146e3c0dcdfcdff564432c34c0f7c1925
bravin-onwonga <onbravin@gmail.com>
2024-01-10 19:16:06 +0300

    Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) != str:
+    if roman_string is None or type(roman_string) is not str:
         return (0)
 
-    my_dict = {'I': 1, 'V': 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+    my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     str_len = len(roman_string)
 
     i = 0
     num = 0
-
     while (i < str_len - 1):
         num1 = my_dict[roman_string[i]]
         num2 = my_dict[roman_string[i + 1]]
@@ -20,6 +19,5 @@ def roman_to_int(roman_string):
 
         i += 1
     num += my_dict[roman_string[i]]
-
 
     return (num)
