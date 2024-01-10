b277e74c8705920d1d318c8e38172530df22c86a
bravin-onwonga <onbravin@gmail.com>
2024-01-10 19:40:39 +0300

    Case for my_list is None

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list is None:
+        return (0)
+
     len_list = len(my_list)
 
     ttl_weight = 0
     divisor = 0
 
-    for i in range (len_list):
-        ttl_weight += my_list[i][0] * my_list[i][1]
+    for i in range(len_list):
+        ttl_weight += (my_list[i][0] * my_list[i][1])
         divisor += my_list[i][1]
 
     return ttl_weight / divisor
