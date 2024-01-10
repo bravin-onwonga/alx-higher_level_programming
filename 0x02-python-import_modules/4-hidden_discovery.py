867911ccfdc9750e195f2adaef19a8c619645c5e
bravin-onwonga <onbravin@gmail.com>
2024-01-11 00:21:23 +0300

    First try

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden
-    defined_modules = [name for name in dir(hidden) if not name.startswith('_')]
+    defined_modules = [name for name in dir(hidden) if not name.startswith('__')]
     defined_modules.sort()
     for s in defined_modules:
         print("{}".format(s))
-
