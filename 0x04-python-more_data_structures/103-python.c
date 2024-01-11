98b3924e86692d4c3aae08d75adf25fd91c998a0
bravin-onwonga <onbravin@gmail.com>
2024-01-11 14:33:52 +0300

    Setting this up

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_python_list - print some basic info about Python lists
+ *
+ * @p: pyobject
+ */
+
+void print_python_list(PyObject *p)
+{
+}
+
+/**
+ * print_python_list - print some basic info about Python objects
+ *
+ * @p: pyobject
+ */
+
+void print_python_bytes(PyObject *p)
+{
+}
