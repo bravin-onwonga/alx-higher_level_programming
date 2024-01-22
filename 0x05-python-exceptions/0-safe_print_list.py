d3ee4537424192089251fe55c4567b0b768f1391
bravin-onwonga <onbravin@gmail.com>
2024-01-22 23:33:50 +0300

    My list can be any type

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         i = 0
-        while(i < x):
+        while (i < x):
             print("{}".format(my_list[i]), end='')
             i += 1
         print()
