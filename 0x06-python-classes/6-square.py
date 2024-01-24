3b973c4f8cb08ec895026b3c610b12fd5bb7cf73
bravin-onwonga <onbravin@gmail.com>
2024-01-24 03:14:05 +0300

    Print a square with spaces

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+"""
+class:
+    Square: A simple class  with a private attribute
+"""
+
+
+class Square:
+    """
+    class:
+        Square: A simple class  with a private attribute
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+        """
+        Attributes:
+            __size: private class attribute for class Square
+            __position: private class attribute for class Square
+        """
+
+    @property
+    def size(self):
+        """
+        Returns: set attribute for class Square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Attributes:
+            value: public instance attribute for class square
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than zero
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+    @property
+    def position(self):
+        """
+        Returns: set attribute for class Square
+        """
+        return self.__size
+
+    @position.setter
+    def position(self, value):
+        """
+        Attributes:
+            value: public instance attribute position for class square
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than zero
+        """
+        if len(value) is not 2 or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """
+        Returns:
+            The square of size
+        """
+        return (self.__size**2)
+
+    def my_print(self):
+        """
+        Prints a square using the symbol #
+        If size is zero then it prints an empty line
+        """
+        i = 0
+
+        if (self.__size == 0):
+            print()
+
+        while (i < self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
+            i += 1
