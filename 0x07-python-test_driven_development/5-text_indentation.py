7204da47e1d753b9b63a40add82b6bbd2f7c9bd3
bravin-onwonga <onbravin@gmail.com>
2024-01-30 19:43:12 +0300

    Split a string

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
+"""
+prints a text with 2 new lines after each of these characters: ., ? and :
+"""
+def text_indentation(text):
+    """
+    Raises:
+        TypeError: text is not a string
+    """
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
 
+    chars = ['.', '?', ':']
+    len_txt = len(text)
+    i = 0
+
+    while(i < len_txt):
+        c = text[i]
+        if c not in chars:
+            if c == ' ' and text[i - 1] not in chars:
+                print(c, end="")
+            if c != ' ':
+                print(c, end="")
+        else:
+            print(c)
+            print()
+        i += 1
