46282fe4df890ea23217b81812f97eea4671c5cb
bravin-onwonga <onbravin@gmail.com>
2024-02-01 03:59:40 +0300

    Queen Puzzle set up

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""The N queens puzzle
+"""
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+N = sys.argv[1]
+
+if not isinstance(N, int):
+    print("N must be a number")
+    sys.exit(1)
+
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(4)
+
+
+def solve_puzzle(N):
+    """solves the N queens problem"""
+    new_matrix = []
+    for i in range(N):
+        for n in range(N):
+            new_matrix.append[i, n]
+        print(new_matrix)
+        new_matrix = []
+
+
