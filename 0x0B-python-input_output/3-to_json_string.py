c0fd20cb2f4367e011bf8a43de2240f80a5f8f70
bravin-onwonga <onbravin@gmail.com>
2024-02-07 00:38:32 +0300

    Serializing an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Returns a json rep of an object"""
+import json
+
+
+def to_json_string(my_obj):
+    """Uses json to convert a object to its string representation
+
+    Parameters:
+        object to serialize
+    Returns:
+        str rep of object
+    """
+    return json.dump(my_obj)
