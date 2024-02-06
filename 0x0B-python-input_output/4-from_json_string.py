cf6922b0442805a863658f830da7eaea52fb63be
bravin-onwonga <onbravin@gmail.com>
2024-02-07 00:43:32 +0300

    Deserializing

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Returns a json rep of an object"""
+import json
+
+
+def from_json_string(my_str):
+    """Uses json to convert a string representation to its object
+
+    Parameters:
+        str to deserialize
+    Returns:
+        object
+    """
+    return json.load(my_str)
