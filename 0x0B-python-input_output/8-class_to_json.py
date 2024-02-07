33720e222c01554b494fb081a8aaa37c30b67a2b
bravin-onwonga <onbravin@gmail.com>
2024-02-07 15:23:56 +0300

    Documenting

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""My class values
+"""
+
+
 def class_to_json(obj):
+    """Returns a dictionary containing class Atrributes and their values"""
     my_dict = {}
-    my_keys = [i for i in obj.__dict__.keys() if i[:1] != '_']
+    my_keys = [i for i in obj.__dict__.keys()]
     my_values = [i for i in obj.__dict__.values()]
 
     for key, value in zip(my_keys, my_values):
