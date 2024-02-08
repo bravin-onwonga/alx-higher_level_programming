31de745c3cb5fd59e920ab991262139a63f05c83
bravin-onwonga <onbravin@gmail.com>
2024-02-08 03:16:44 +0300

    Is it an instance of?

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Check if an obj is an instance of a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """Returns whether an obj is an instance of a class or not
+    """
+    return isinstance(obj, a_class)
