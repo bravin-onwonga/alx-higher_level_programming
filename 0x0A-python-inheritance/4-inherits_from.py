ac01251e3c2216b422d14968dee86a8a679b5f57
bravin-onwonga <onbravin@gmail.com>
2024-02-08 03:40:37 +0300

    Has obj inherited from class?

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Checks if an object is an instance of a class that inherited
+    (directly or indirectly) from the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True is an object inherited from a class; otherwise False
+
+    Parameters:
+        obj - object
+        a_class - class info
+    """
+    return issubclass(obj, a_class)
