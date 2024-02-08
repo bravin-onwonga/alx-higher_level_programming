1f95667debff4f277446fc9fc0bc96ff5ba67d37
bravin-onwonga <onbravin@gmail.com>
2024-02-08 04:02:28 +0300

    Area raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Creates a simple class
+"""
+
+
+class BaseGeometry:
+    """A simple class that does nothing"""
+    def area(self):
+        """A method that does nothing"""
+        raise Exception("area() is not implemented")
