f63f651eddb463349162b6cdba3aea1c6db9f59c
bravin-onwonga <onbravin@gmail.com>
2024-02-12 15:50:49 +0300

    Save to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,3 +30,33 @@ class Base:
             key = my_lst[i]
             new_dict[key] = my_dict[key]
         return json.dumps([new_dict])
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        import json
+        txt = "["
+        len_list_objs = len(list_objs)
+
+        i = 0
+        while(i < len_list_objs):
+            content = list_objs[i].to_dictionary()
+
+            my_lst = ["y", "x", "id", "width", "height"]
+            new_dict = {}
+            for n in range(0, len(my_lst)):
+                key = my_lst[n]
+                new_dict[key] = content[key]
+
+            hold_content = json.dumps(new_dict)
+
+            txt += hold_content
+            if i != (len_list_objs - 1):
+                txt += ", "
+            else:
+                txt += "]"
+            i += 1
+            print(i)
+
+        with open("Rectangle.json", 'w') as my_file:
+            my_file.write(txt)
