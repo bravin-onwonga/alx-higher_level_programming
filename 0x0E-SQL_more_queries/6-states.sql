58ddb65a307a196c43727fe5c2cfb0243786d44b
bravin-onwonga <onbravin@gmail.com>
2024-03-15 00:41:38 +0300

    Should be unique

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,12 @@
 -- Creates the database hbtn_0d_usa and the table states init
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Switches to hbtn_0d_usa database
 USE hbtn_0d_usa;
 
+-- Creates table with auto_increment key
 CREATE TABLE IF NOT EXISTS states (
-	id INT DEFAULT 1 NOT NULL UNIQUE,
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY(id)
 );
