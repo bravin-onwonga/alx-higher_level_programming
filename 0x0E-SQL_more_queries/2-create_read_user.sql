8350e8fda2965912a0ac4245cb32bac558d65d3a
bravin-onwonga <onbravin@gmail.com>
2024-03-14 21:25:49 +0300

    Create user and database then grant privileges

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- Creates a new user user_0d_2 and a new database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+INDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT
+   ON `hbtn_0d_2`
+   TO 'user_0d_2'@'localhost'
+   WITH GRANT OPTION;
