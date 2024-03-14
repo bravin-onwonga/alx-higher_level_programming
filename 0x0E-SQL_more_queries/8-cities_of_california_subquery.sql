5ec77085628b422fc99eac797f3abdf9003e4411
bravin-onwonga <onbravin@gmail.com>
2024-03-15 02:24:09 +0300

    fix:states not state

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_us
 SELECT id, name
-FROM cities, state
+FROM cities, states
 ON cities.state_id = state.id
-WHERE state.name = 'California';
+WHERE states.name = 'California';
