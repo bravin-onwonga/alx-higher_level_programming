025c6519873e94b3eeae8408d2f5d75961a3c3b2
bravin-onwonga <onbravin@gmail.com>
2024-03-14 21:45:21 +0300

    Removing backticks

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,5 +5,6 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 INDENTIFIED BY 'user_0d_2_pwd';
 
 GRANT SELECT
-   ON `hbtn_0d_2`
-   TO 'user_0d_2'@'localhost';
+ON hbtn_0d_2
+TO 'user_0d_2'@'localhost'
+WITH GRANT OPTION;
