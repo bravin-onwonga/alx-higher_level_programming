b163bbf81f5f552543aa75e309bec7a20bdda268
bravin-onwonga <onbravin@gmail.com>
2024-03-15 01:37:39 +0300

    fix:Adding where condition

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_us
 SELECT id, name
 FROM cities, state
-WHERE cities.state_id = state.id;
+WHERE state.name = 'Carlifonia'
+AND cities.state_id = state.id;
