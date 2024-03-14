997c006a68943d65b5d7e4c3ab8ba3256ef5336b
bravin-onwonga <onbravin@gmail.com>
2024-03-14 21:53:48 +0300

    fix:Spelling error -> IDENTIFIED

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
-INDENTIFIED BY 'user_0d_2_pwd';
+IDENTIFIED BY 'user_0d_2_pwd@1';
 
 GRANT SELECT
 ON hbtn_0d_2
