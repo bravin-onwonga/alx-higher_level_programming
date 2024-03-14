6c8d7de31142cb9f61489b46baa4587243fa8e59
bravin-onwonga <onbravin@gmail.com>
2024-03-14 19:26:24 +0300

    Grants all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,6 +3,6 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_Pwd@';
 
 GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   WITH GRANT OPTION;
+ON *.*
+TO 'user_0d_1'@'localhost'
+WITH GRANT OPTION;
