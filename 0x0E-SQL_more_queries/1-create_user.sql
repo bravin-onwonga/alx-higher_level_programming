e88076016e3e605131914a5dfae62ad9f753b30f
bravin-onwonga <onbravin@gmail.com>
2024-03-14 19:19:13 +0300

    Grants all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Creates the MySQL server user user_0d_1 and grants priviledges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_Pwd@';
+
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   WITH GRANT OPTION;
