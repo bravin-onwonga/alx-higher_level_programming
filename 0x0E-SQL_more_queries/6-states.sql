7f81d158465379572cddf5b98b67f9f1663368ba
bravin-onwonga <onbravin@gmail.com>
2024-03-15 00:51:17 +0300

    fix:Adding unique to pk

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
--- Creates the database hbtn_0d_usa and the table states init
+-- Creates the database hbtn_0d_usa and the table states in it
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- Switches to hbtn_0d_usa database
