ce3517b1fe12e8198fc1df3ba0a69ed7d4d9cdb3
bravin-onwonga <onbravin@gmail.com>
2024-03-16 02:06:12 +0300

    fix:Using having to filter those with zero

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,8 @@
 -- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT DISTINCT tg.name AS genre, COUNT(tsg.genre_id) AS number_of_shows
+SELECT tg.name AS genre, COUNT(tsg.genre_id) AS number_of_shows
 FROM tv_genres AS tg
 LEFT JOIN tv_show_genres AS tsg
 ON tg.id = tsg.genre_id
 GROUP BY genre
+HAVING COUNT(tsg.genre_id) <> 0
 ORDER BY number_of_shows DESC;
