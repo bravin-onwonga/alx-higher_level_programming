0ff99204e22356d53671faf9595155449c04e1ad
bravin-onwonga <onbravin@gmail.com>
2024-03-16 01:56:38 +0300

    fix:Adding distinct keyword

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT tg.name AS genre, COUNT(tsg.genre_id) AS number_of_shows
+SELECT DISTINCT tg.name AS genre, COUNT(tsg.genre_id) AS number_of_shows
 FROM tv_genres AS tg
 LEFT JOIN tv_show_genres AS tsg
 ON tg.id = tsg.genre_id
