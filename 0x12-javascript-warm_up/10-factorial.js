60a0e46a11357ad18f98516499d26a0f9506e519
bravin-onwonga <onbravin@gmail.com>
2024-04-09 03:03:51 +0300

    Calculating the factorial of a number recursively

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+if (isNaN(num)) {
+  console.log(1);
+} else {
+  function factorial (num) {
+    if (num < 0) {
+      return (-1);
+    }
+
+    if (num <= 1) {
+      return (1);
+    }
+
+    return (num * factorial(num - 1));
+  }
+
+  console.log(factorial(num));
+}
