46900f4296244af95700a63dec1b46110a2af9ef
bravin-onwonga <onbravin@gmail.com>
2024-04-10 04:24:40 +0300

    Read and write to local files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const firstFile = process.argv[2];
+const secondFile = process.argv[3];
+const destination = process.argv[4];
+
+fs.readFile(firstFile, 'utf8', function (err, dataFirst) {
+  if (err) {
+    throw err;
+  }
+
+  fs.readFile(secondFile, 'utf8', function (err, dataSecond) {
+    if (err) {
+      throw err;
+    }
+
+    const fullText = dataFirst + '\n' + dataSecond;
+
+    fs.writeFile(destination, fullText, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  });
+});
