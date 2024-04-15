7a84d43e496de8a48487ad391ba106c3d38b4eb6
bravin-onwonga <onbravin@gmail.com>
2024-04-15 15:06:47 +0300

    Using MySQLdb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,6 +4,7 @@ Module to list state.name ordered by id
 """
 
 import sys
+import MySQLdb
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
