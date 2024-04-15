23f7940dfceced95c020eff9f53ef4c65c5cac34
bravin-onwonga <onbravin@gmail.com>
2024-04-15 18:06:38 +0300

    Safe from sql injection

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,7 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    username, password, dbName = sys.argv[1:4]
-    state_name = sys.argv[4]
+    username, password, dbName, state_name = sys.argv[1:]
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=dbName, charset="utf8")
     cur = conn.cursor()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,9 +12,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=dbName, charset="utf8")
     cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' \
-        ORDER BY id ASC".format(state_name)
-    cur.execute(query)
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
