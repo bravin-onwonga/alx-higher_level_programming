531c396de8d8e410b3091b06cf86dc117470298a
bravin-onwonga <onbravin@gmail.com>
2024-04-15 15:28:05 +0300

    Only those beginnning with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module to list state.name ordered by id
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username, password, dbName = sys.argv[1:]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=dbName, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1][0] == 'N':
+            print(row)
+    cur.close()
+    conn.close()
