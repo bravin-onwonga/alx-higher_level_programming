5b4b695fe4965b8aa4df7d3364f2123e070ea29e
bravin-onwonga <onbravin@gmail.com>
2024-04-15 20:02:42 +0300

    Fixing pycodestyle

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,10 +11,11 @@ if __name__ == "__main__":
     if (len(sys.argv) == 5):
         username, password, dbName, state_name = sys.argv[1:]
         conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                            passwd=password, db=dbName, charset="utf8")
+                               passwd=password, db=dbName, charset="utf8")
         cur = conn.cursor()
         query = "SELECT name FROM cities WHERE cities.state_id = \
-            (SELECT id FROM states WHERE states.name = %s) ORDER BY cities.id ASC"
+            (SELECT id FROM states WHERE states.name = %s) \
+                ORDER BY cities.id ASC"
         cur.execute(query, (state_name,))
         query_rows = cur.fetchall()
         for i in range(len(query_rows) - 1):
