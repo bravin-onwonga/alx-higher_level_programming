bf3aafdcdc8337225d30fd89d833035a276305ac
bravin-onwonga <onbravin@gmail.com>
2024-04-15 15:03:49 +0300

    Fixing pycodestyle error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@
 Module to list state.name ordered by id
 """
 
-import MySQLdb, sys
+import sys
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
