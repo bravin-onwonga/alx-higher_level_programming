b54da4aa979c3f3debf2b5a0d61596a9c1128cba
bravin-onwonga <onbravin@gmail.com>
2024-04-16 02:17:56 +0300

    Delete those with an a

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module to list state.name ordered by id
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, delete
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+if __name__ == "__main__":
+    username, passwd, dbName = sys.argv[1:]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (username, passwd, dbName), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).delete()
+
+    session.commit()
+
+    session.close()
