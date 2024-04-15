ee3edeaaf4e60d3c2399a071f556ca54e9f9dd56
bravin-onwonga <onbravin@gmail.com>
2024-04-15 20:32:45 +0300

    First State object

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module to list state.name ordered by id
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+if __name__ == "__main__":
+    username, passwd, dbName = sys.argv[1:]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (username, passwd, dbName), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(state.id, state.name))
+
+    session.close()
