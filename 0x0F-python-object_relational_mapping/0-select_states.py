a690d5319fba3aa3ef6af129b8aab3238f65fde8
bravin-onwonga <onbravin@gmail.com>
2024-04-15 15:15:11 +0300

    Using MySQLdb not ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,34 +5,16 @@ Module to list state.name ordered by id
 
 import sys
 import MySQLdb
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-
-class State(Base):
-    """
-    class representing our table states
-    """
-    __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
 
 
 if __name__ == "__main__":
-    username, passwd, dbName = sys.argv[1:]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (username, passwd, dbName), pool_pre_ping=True)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-
-    session.close()
+    username, password, dbName = sys.argv[1:]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=dbName, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
