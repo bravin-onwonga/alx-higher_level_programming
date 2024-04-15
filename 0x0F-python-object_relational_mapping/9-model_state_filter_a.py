51612c54c0451eb42991cafd06f5169e5e865fe4
bravin-onwonga <onbravin@gmail.com>
2024-04-15 23:23:16 +0300

    Filter for character a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module to list state.name ordered by id
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+if __name__ == "__main__":
+    username, passwd, dbName = sys.argv[1:]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (username, passwd, dbName), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).\
+        order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
