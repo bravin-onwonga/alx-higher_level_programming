717b1c5c553a7e9480c9ceab14953f4f1840667f
bravin-onwonga <onbravin@gmail.com>
2024-04-15 19:35:23 +0300

    Cities from state name

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module to list row based on arg passed
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username, password, dbName, state_name = sys.argv[1:]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=dbName, charset="utf8")
+    cur = conn.cursor()
+    query = "SELECT name FROM cities WHERE cities.state_id = \
+        (SELECT id FROM states WHERE states.name = %s) ORDER BY cities.id ASC"
+    cur.execute(query, (state_name,))
+    query_rows = cur.fetchall()
+    for i in range(len(query_rows) - 1):
+        print("{}".format(query_rows[i][0]), end=", ")
+    if (len(query_rows) > 0):
+        print("{}".format(query_rows[len(query_rows) - 1][0]))
+    cur.close()
+    conn.close()
