28a4f459594f6f58250bd49f39e8fdcf4c8f74e5
bravin-onwonga <onbravin@gmail.com>
2024-04-28 00:31:16 +0300

    Post using requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,8 +9,7 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    req = requests.post(url, data={'email': email})
-    with request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-
-    print(body)
+    data={'email': email}
+    req = requests.post(url, json = data)
+    res = requests.get(url)
+    print(res.text)
