7616c56ed63fce7afb6962575aabe3448601bb5d
bravin-onwonga <onbravin@gmail.com>
2024-04-27 03:35:34 +0300

    fix:Importing only request package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,13 +2,12 @@
 """
 Fetches https://alx-intranet.hbtn.io/status using requests package
 """
-from urllib import request
+import requests
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    with request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", body.decode('utf-8'))
+    res = requests.get(url)
+    body = res.text
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
