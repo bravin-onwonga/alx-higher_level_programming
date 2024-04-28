05f71781b67f05ddec7dc5a04fc15f908a63ddd2
bravin-onwonga <onbravin@gmail.com>
2024-04-29 02:06:11 +0300

    fix:First and second arguments(repo, owner)

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,14 +9,14 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    repo = sys.argv[2]
+    repo = sys.argv[1]
+    owner= sys.argv[2]
     res = requests.get("https://api.github.com/repos/{}/{}/commits"
-                       .format(username, repo))
+                       .format(owner, repo))
     data = res.json()
     i = 0
     while (i < 10):
         sha = data[i].get('sha')
-        name = data[i].get('commit').get('committer').get('name')
+        name = data[i].get('commit').get('author').get('name')
         print("{}: {}".format(sha, name))
         i += 1
