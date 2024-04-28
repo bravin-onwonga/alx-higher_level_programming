b5b059b578f49ec7311ae3b6d6643a40b78f1109
bravin-onwonga <onbravin@gmail.com>
2024-04-28 15:22:50 +0300

    Using json

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {'email': email}
-    res = requests.post(url, json=data)
-    print(res.json())
+    res = requests.post(url, data=data)
+    print(res.text)
