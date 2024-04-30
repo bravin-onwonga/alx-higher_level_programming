7650a7f0c2992bf3127b9501c6d5387d407bd14f
bravin-onwonga <onbravin@gmail.com>
2024-04-30 15:59:11 +0300

    fix:Writing it all on one line

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display only body of a 200 status code response
-curl -s -X GET "$1"
+status_code=$(curl -s -o /dev/null -w "%{http_code}" -X GET "$1"); if ["$status_code" -eq 200]; then curl -s -X GET "$1"; fi
