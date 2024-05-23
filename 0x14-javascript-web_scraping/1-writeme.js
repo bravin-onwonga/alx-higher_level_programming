b0b92c866acfaaae784024c265f25d2c768a7b63
bravin-onwonga <onbravin@gmail.com>
2024-05-23 17:37:46 +0300

    Write to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const fileName = args[2];
+const content = args[3];
+
+const fs = require('fs');
+
+fs.writeFile(fileName, content, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+});
